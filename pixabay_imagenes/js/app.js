7bfd54ffc58a80ea7800d7094b085fa16f8cdb8c
Koridormi <258958198+Koridormi@users.noreply.github.com>
2026-03-29 16:50:09 -0300

    Async-Await Added

--- pixabay_imagenes/js/app.js
+++ pixabay_imagenes/js/app.js
@@ -38,19 +38,19 @@ function mostrarAlerta(mensaje) {
     };
 };
 
-function buscarImagenes() {
+async function buscarImagenes() {
     const termino = document.querySelector('#termino').value;
 
-    const key = 'Your_Api_Key_Here'; // <-------
+    const key = 'Your_Api_Key_Here'; // <------- Your_Api_Key_Here
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registroPorPagina}&page=${paginaActual}`;
-    fetch(url)
-        .then( (respuesta) => {
-            return respuesta.json();
-        })
-        .then( (resultado) => {
-            totalPaginas = calcularPaginas(resultado.totalHits);
-            mostrarImagenes(resultado.hits);
-        })
+    try {
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+      totalPaginas = calcularPaginas(resultado.totalHits);
+      mostrarImagenes(resultado.hits);
+    } catch (error) {
+        console.log(error);
+    };
 };
 
 // generador que va a registrar la cantidad de elementos de acuerdo a las paginas
